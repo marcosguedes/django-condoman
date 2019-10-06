9564d7c2fe530be35e2180a52a91c337a421318f
Marcos Guedes <marcosguedes@users.noreply.github.com>
2019-10-06 12:25:04 +0100

    updated admin for dues and payments

--- src/dues/admin.py
+++ src/dues/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BankTransferDue, ExemptDue
+
+
+@admin.register(BankTransferDue)
+class BankTransferDueAdmin(admin.ModelAdmin):
+    list_display = ("name", "proprietor", "payment", "payment_date_limit")
+
+
+@admin.register(ExemptDue)
+class ExemptDueAdmin(admin.ModelAdmin):
+    list_display = ("name", "proprietor", "payment")
--- src/payments/admin.py
+++ src/payments/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Payment
 
-admin.site.register(Payment, admin.ModelAdmin)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("billing_name", "description", "status")
